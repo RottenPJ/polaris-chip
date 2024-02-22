9ce5242b0fac29424a8a65a621db3185e3df67e8
RottenPJ <pjw5488@psu.edu>
2024-02-22 12:48:04 -0500

    add warning app frame

---
+++ src/warning-app.js
@@ -0,0 +1,40 @@
+import { LitElement, html, css } from 'lit';
+
+export class WarningApp extends LitElement {
+  static get tag() {
+    return 'warning-app';
+  }
+
+
+  static get properties() {
+    return {
+      
+    };
+  }
+
+  constructor() {
+    super();
+    
+  }
+
+  static get styles() {
+    return css`
+      
+    `;
+  }
+
+  render() {
+    return html`
+    
+  
+    `;
+  }
+
+  
+
+  
+
+ 
+}
+
+customElements.define(WarningApp.tag, WarningApp);
